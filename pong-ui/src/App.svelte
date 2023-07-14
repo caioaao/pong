0bf9dd2a080598902436b92c39fcb3ca62b0624d
Caio Oliveira <caioaao@gmail.com>
2023-07-14 09:38:48 -0300

    control pad with arrow keys

--- pong-ui/src/App.svelte
+++ pong-ui/src/App.svelte
@@ -1,5 +1,22 @@
 <script lang="ts">
 	import GameScene from "./lib/GameScene.svelte";
+
+	let padPosX = 50;
+
+	function onKeyDown(e: KeyboardEvent) {
+		switch (e.code) {
+			case "ArrowLeft":
+			case "a":
+				padPosX = Math.max(10, padPosX - 5);
+				break;
+			case "ArrowRight":
+			case "d":
+				padPosX = Math.min(90, padPosX + 5);
+				break;
+		}
+	}
 </script>
 
-<GameScene />
+<GameScene padPos={{ x: padPosX, y: 10 }} />
+
+<svelte:window on:keydown|preventDefault={onKeyDown} />
