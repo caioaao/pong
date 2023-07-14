9d878e10e5608feb0f3fb9c7081d077c38c6492f
Caio Oliveira <caioaao@gmail.com>
2023-07-14 08:27:36 -0300

    refactor game scene

--- pong-ui/src/lib/GameScene.svelte
+++ pong-ui/src/lib/GameScene.svelte
@@ -1,41 +1,38 @@
 <script lang="ts">
-	export let viewportWidth = 600;
-	export let viewportHeight = 600;
+	// export let ballPos = { x: 50, y: 50 };
+	export let padPos = { x: 50, y: 10 };
 
-	// measured in percent (from 0 to 100)
-	export let playerBarPosition = 0;
+	const viewportWidth = 600;
+	const viewportHeight = 600;
 
-	$: padWidth = viewportWidth / 4.5;
-	$: padHeight = viewportHeight / 30;
-	$: padRailWidth = viewportWidth * 0.9;
-	$: barPosX = (playerBarPosition * (padRailWidth - padWidth)) / 100;
-	$: padRailHMargin = (viewportWidth - padRailWidth) / 2;
+	const absPadWidth = 20;
+	const absPadHeight = 2;
+
+	function scaleX(x: number): number {
+		return (x * viewportWidth) / 100;
+	}
+
+	function scaleY(Y: number): number {
+		return (Y * viewportHeight) / 100;
+	}
+
+	$: padWidth = scaleX(absPadWidth);
+	$: padHeight = scaleX(absPadHeight);
+	$: padLeft = scaleX(padPos.x) - scaleX(absPadWidth) / 2;
+	$: padBottom = scaleY(padPos.y - absPadHeight);
 </script>
 
 <main style="width:{viewportWidth}px; height:{viewportHeight}px">
 	<div
-		class="pad-rail"
-		style="height: {padHeight}px; 
-		width: {padRailWidth}px;
-		margin-left: {padRailHMargin}px;
-		margin-right: {padRailHMargin}px;
-		bottom: {padHeight * 2}px"
-	>
-		<div
-			class="pad"
-			style="left: {barPosX}px; width: {padWidth}px; height: {padHeight}px"
-		/>
-	</div>
+		class="pad"
+		style="left: {padLeft}px; bottom: {padBottom}px; width: {padWidth}px; height: {padHeight}px"
+	/>
 </main>
 
 <style>
 	main {
 		position: relative;
 		background-color: black;
-	}
-
-	.pad-rail {
-		position: absolute;
 	}
 
 	.pad {
