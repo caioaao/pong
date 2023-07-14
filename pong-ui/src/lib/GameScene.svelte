da9c7ab073bbdbeea2d6a715ca823657f58c06bf
Caio Oliveira <caioaao@gmail.com>
2023-07-14 08:34:41 -0300

    add ball

--- pong-ui/src/lib/GameScene.svelte
+++ pong-ui/src/lib/GameScene.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	// export let ballPos = { x: 50, y: 50 };
+	export let ballPos = { x: 50, y: 50 };
 	export let padPos = { x: 50, y: 10 };
 
 	const viewportWidth = 600;
@@ -7,6 +7,7 @@
 
 	const absPadWidth = 20;
 	const absPadHeight = 2;
+	const absBallRadius = 5;
 
 	function scaleX(x: number): number {
 		return (x * viewportWidth) / 100;
@@ -18,14 +19,29 @@
 
 	$: padWidth = scaleX(absPadWidth);
 	$: padHeight = scaleX(absPadHeight);
-	$: padLeft = scaleX(padPos.x) - scaleX(absPadWidth) / 2;
-	$: padBottom = scaleY(padPos.y - absPadHeight);
+	$: padLeft = scaleX(padPos.x) - padWidth / 2;
+	$: padBottom = scaleY(padPos.y) - padHeight;
+
+	$: ballRadius = scaleX(absBallRadius);
+	$: ballLeft = scaleX(ballPos.x) - ballRadius;
+	$: ballBottom = scaleX(ballPos.y) - ballRadius;
 </script>
 
 <main style="width:{viewportWidth}px; height:{viewportHeight}px">
 	<div
 		class="pad"
-		style="left: {padLeft}px; bottom: {padBottom}px; width: {padWidth}px; height: {padHeight}px"
+		style="left: {padLeft}px;
+		bottom: {padBottom}px; 
+		width: {padWidth}px; 
+		height: {padHeight}px;"
+	/>
+	<div
+		class="ball"
+		style="width: {ballRadius * 2}px; 
+		height: {ballRadius * 2}px;
+		left:{ballLeft}px;
+		bottom: {ballBottom}px;
+		border-radius: {ballRadius}px"
 	/>
 </main>
 
@@ -36,6 +52,11 @@
 	}
 
 	.pad {
+		position: absolute;
+		background-color: white;
+	}
+
+	.ball {
 		position: absolute;
 		background-color: white;
 	}
