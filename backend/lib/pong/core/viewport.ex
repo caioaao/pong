03ebb0b62b41d6c6a613e4fe279a5b2d9ddd7bd6
Caio Oliveira <caioaao@gmail.com>
2023-07-16 22:43:57 -0300

    viewport edges

--- backend/lib/pong/core/viewport.ex
+++ backend/lib/pong/core/viewport.ex
@@ -1,4 +1,8 @@
 defmodule Pong.Core.Viewport do
   def width, do: 100
   def height, do: 100
+  def left_edge, do: {{0, 0}, {0, height()}}
+  def right_edge, do: {{width(), 0}, {width(), height()}}
+  def top_edge, do: {{0, height()}, {width(), height()}}
+  def bottom_edge, do: {{0, 0}, {width(), 0}}
 end
