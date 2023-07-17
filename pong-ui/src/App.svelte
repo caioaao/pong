3e42766934321eb56785fa7c83164231c73bc625
Caio Oliveira <caioaao@gmail.com>
2023-07-17 10:08:13 -0300

    nice looking header + score

--- pong-ui/src/App.svelte
+++ pong-ui/src/App.svelte
@@ -29,10 +29,45 @@
 	}
 </script>
 
-<GameScene>
-	<Player1Pad viewportSize={600} />
-	<Player2Pad viewportSize={600} />
-	<Ball viewportSize={600} />
-</GameScene>
+<main>
+	<header>
+		<h1>Pong</h1>
+		<div class="score">0 - 0</div>
+	</header>
+	<GameScene>
+		<Player1Pad viewportSize={600} />
+		<Player2Pad viewportSize={600} />
+		<Ball viewportSize={600} />
+	</GameScene>
+</main>
 
 <svelte:window on:keydown={onKeyDown} />
+
+<style>
+	header {
+		position: relative;
+		width: 100%;
+		display: flex;
+		align-items: baseline;
+	}
+
+	header h1 {
+		text-align: left;
+		text-transform: uppercase;
+		font-weight: 900;
+		font-family: Avenir, Montserrat, Corbel, "URW Gothic",
+			source-sans-pro, sans-serif;
+		flex: 1;
+		margin: 0;
+		font-size: 3.2em;
+	}
+
+	header .score {
+		flex: 1;
+		text-align: right;
+		font-weight: 900;
+		font-family: Avenir, Montserrat, Corbel, "URW Gothic",
+			source-sans-pro, sans-serif;
+		font-size: 3.2em;
+	}
+</style>
