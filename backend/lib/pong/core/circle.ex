72e4f46395973e295b7bf3e2553b3441a9c5b087
Caio Oliveira <caioaao@gmail.com>
2023-07-16 22:43:41 -0300

    remove unused function to avoid confusion

--- backend/lib/pong/core/circle.ex
+++ backend/lib/pong/core/circle.ex
@@ -1,5 +1,5 @@
 defmodule Pong.Core.Circle do
-  alias Pong.Core.{Point, LineSegment, Rectangle, Vector, Viewport}
+  alias Pong.Core.{Point, LineSegment, Rectangle, Vector}
 
   @type t() :: %{radius: number(), center: Point.t()}
 
@@ -17,15 +17,5 @@ defmodule Pong.Core.Circle do
   @spec move(t(), Vector.t()) :: t()
   def move(circle, vec) do
     update_in(circle, [:center], &Vector.add(&1, vec))
-  end
-
-  @spec ensure_inside_viewport(t()) :: t()
-  def ensure_inside_viewport(circle) do
-    update_in(circle, [:center], fn {x, y} ->
-      {
-        x |> min(Viewport.width() - circle[:radius] / 2) |> max(circle[:radius] / 2),
-        y |> min(Viewport.height() - circle[:radius] / 2) |> max(circle[:radius] / 2)
-      }
-    end)
   end
 end
