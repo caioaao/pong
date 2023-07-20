a0968b7926657fbe2cc5553d89703590ed57db90
Caio Oliveira <caioaao@gmail.com>
2023-07-20 18:55:55 -0300

    more styling and placeholder buttons

--- pong-ui/src/routes/+page.svelte
+++ pong-ui/src/routes/+page.svelte
@@ -2,10 +2,13 @@
 	<h1>PONG</h1>
 	<ul>
 		<li>
-			<a href="/">Home</a>
+			<a href="/single-player"> Single Player </a>
 		</li>
 		<li>
-			<a href="/single-player"> Single Player </a>
+			<a href="/multiplayer/new-match"> Create Match </a>
+		</li>
+		<li>
+			<a href="/multiplayer/join-match"> Join Match </a>
 		</li>
 	</ul>
 </nav>
@@ -13,8 +16,18 @@
 <style>
 	nav > ul {
 		list-style: none;
+		display: grid;
+		row-gap: 10px;
 	}
 	nav > ul > li {
 		font-size: 2rem;
+	}
+	li > a {
+		display: block;
+		padding: 1rem;
+		width: 300px;
+		background-color: #eeeeee;
+		color: #333;
+		text-transform: uppercase;
 	}
 </style>
