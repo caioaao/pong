900109285ed6899448a142d7061d8a03e1560b74
Caio Oliveira <caioaao@gmail.com>
2023-07-20 18:46:45 -0300

    extract game component

--- pong-ui/src/routes/+page.svelte
+++ pong-ui/src/routes/+page.svelte
@@ -1,4 +1,21 @@
 <nav>
-	<a href="/">Home</a>
-	<a href="/single-player"> Single Player </a>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/single-player"> Single Player </a>
+		</li>
+	</ul>
 </nav>
+
+<style>
+	nav > ul {
+		list-style: none;
+		text-align: left;
+	}
+	nav > ul > li {
+		width: 300px;
+		font-size: 2rem;
+	}
+</style>
