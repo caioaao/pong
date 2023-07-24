1479adc72b7a8b4e74a40fbabae0c8f64c4063e3
Caio Oliveira <caioaao@gmail.com>
2023-07-24 16:31:18 -0300

    `@impl true`

--- backend/lib/pong/api/http/dummy_web_socket_handler.ex
+++ backend/lib/pong/api/http/dummy_web_socket_handler.ex
@@ -10,10 +10,12 @@ defmodule Pong.Api.Http.DummyWebSocketHandler do
     {:player_request, :move_pad_right, :player2}
   ]
 
+  @impl true
   def init(request, state) do
     {:cowboy_websocket, request, state}
   end
 
+  @impl true
   def websocket_init(_state) do
     with {:ok, server} <- MatchServer.start_link([]) do
       MatchServer.subscribe(server, MatchSubscription.child_spec(self()))
@@ -27,30 +29,36 @@ defmodule Pong.Api.Http.DummyWebSocketHandler do
   end
 
   # heartbeat - ignore
+  @impl true
   def websocket_handle({:text, ""}, server) do
     {:ok, server}
   end
 
+  @impl true
   def websocket_handle({:text, "move_left"}, server) do
     MatchServer.process_event(server, {:player_request, :move_pad_left, :player1})
     {:ok, server}
   end
 
+  @impl true
   def websocket_handle({:text, "move_right"}, server) do
     MatchServer.process_event(server, {:player_request, :move_pad_right, :player1})
     {:ok, server}
   end
 
+  @impl true
   def websocket_handle({:text, "pause"}, server) do
     MatchServer.process_event(server, {:player_request, :pause, :player1})
     {:ok, server}
   end
 
+  @impl true
   def websocket_handle({:text, "unpause"}, server) do
     MatchServer.process_event(server, {:player_request, :unpause, :player1})
     {:ok, server}
   end
 
+  @impl true
   def websocket_handle({:text, msg}, server) do
     Logger.warn("unknown message #{msg} received - echoing to client")
     {:reply, {:text, msg}, server}
@@ -62,10 +70,12 @@ defmodule Pong.Api.Http.DummyWebSocketHandler do
     {:ok, server}
   end
 
+  @impl true
   def websocket_info({:match_state, match}, server) do
     reply_with_match(match, server)
   end
 
+  @impl true
   def websocket_info(_, state) do
     {:ok, state}
   end
