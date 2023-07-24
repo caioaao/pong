324e31282b25d601eab1c028008aed168fd95175
Caio Oliveira <caioaao@gmail.com>
2023-07-24 16:22:00 -0300

    use new registry in multiplayer socket handle

--- backend/lib/pong/api/http/web_socket_handler.ex
+++ backend/lib/pong/api/http/web_socket_handler.ex
@@ -2,63 +2,76 @@ defmodule Pong.Api.Http.WebSocketHandler do
   @behaviour :cowboy_websocket
 
   alias Pong.Core.Match.Server, as: MatchServer
+  alias Pong.Core.Match.Registry.V2, as: MatchRegistry
   require Logger
 
-  def init(request, _state) do
-    # FIXME 
-    with ["", "ws", match_id, player_id] <- String.split(request.path, "/") do
-      case player_id do
-        "player1" ->
-          {:cowboy_websocket, request, %{match_id: match_id, player: :player1}}
+  @impl true
+  def init(request, _) do
+    %{player: username, match: match_id} = :cowboy_req.match_qs([:player, :match], request)
 
-        "player2" ->
-          {:cowboy_websocket, request, %{match_id: match_id, player: :player2}}
-      end
+    case MatchRegistry.lookup(match_id) do
+      {_, match_pid, %{player1: ^username}} ->
+        {:cowboy_websocket, request, %{match_pid: match_pid, player: :player1}}
+
+      {_, match_pid, %{player2: ^username}} ->
+        {:cowboy_websocket, request, %{match_pid: match_pid, player: :player2}}
+
+      # TODO join as spectator
+      _ ->
+        {:error, :player_not_in_match}
     end
   end
 
-  def websocket_init(%{match_id: match_id} = state) do
-    match_server = Pong.Core.Match.Registry.get_or_create_match(match_id)
-    ref = Process.monitor(match_server)
+  @impl true
+  def websocket_init(%{match_pid: match_pid} = state) do
+    ref = Process.monitor(match_pid)
 
     Process.send(self(), :poll_state, [])
-    {:ok, Map.merge(state, %{match_pid: match_server, match_ref: ref})}
+    {:ok, Map.put(state, :match_ref, ref)}
   end
 
+  @impl true
   # heartbeat - ignore
   def websocket_handle({:text, ""}, server) do
     {:ok, server}
   end
 
+  @impl true
   def websocket_handle({:text, "move_left"}, %{player: player, match_pid: match_pid} = state) do
     MatchServer.process_event(match_pid, {:player_request, :move_pad_left, player})
     {:ok, state}
   end
 
+  @impl true
   def websocket_handle({:text, "move_right"}, %{player: player, match_pid: match_pid} = state) do
     MatchServer.process_event(match_pid, {:player_request, :move_pad_right, player})
     {:ok, state}
   end
 
+  @impl true
   def websocket_handle({:text, "pause"}, %{player: player, match_pid: match_pid} = state) do
     MatchServer.process_event(match_pid, {:player_request, :pause, player})
     {:ok, state}
   end
 
+  @impl true
   def websocket_handle({:text, "unpause"}, %{player: player, match_pid: match_pid} = state) do
     MatchServer.process_event(match_pid, {:player_request, :unpause, player})
     {:ok, state}
   end
 
+  @impl true
   def websocket_handle({:text, msg}, server) do
     Logger.warn("unknown message #{msg} received - echoing to client")
     {:reply, {:text, msg}, server}
   end
 
+  @impl true
   def websocket_info({:DOWN, ref, :process, _, _}, %{match_ref: ref} = state) do
     {:stop, state}
   end
 
+  @impl true
   def websocket_info(:poll_state, %{match_pid: match_pid} = state) do
     Process.send_after(self(), :poll_state, div(1000, 60))
 
@@ -75,6 +88,7 @@ defmodule Pong.Api.Http.WebSocketHandler do
     end
   end
 
+  @impl true
   def websocket_info(info, state) do
     {:reply, {:text, info}, state}
   end
