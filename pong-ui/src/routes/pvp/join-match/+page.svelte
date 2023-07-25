79d3f38d1197c858da7f5491292e9584432a72e9
Caio Oliveira <caioaao@gmail.com>
2023-07-25 10:32:16 -0300

    add `join` screen

---
+++ pong-ui/src/routes/pvp/join-match/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	let matchID: string;
+</script>
+
+<main>
+	<h1>PONG</h1>
+	<label for="match-id">Match ID</label>
+	<input type="text" id="match-id" bind:value={matchID} />
+	<a class="join-button" href={matchID ? `/pvp/join-match/${matchID}` : ''}> Join </a>
+</main>
+
+<style>
+	main {
+		width: 600px;
+		display: flex;
+		flex-direction: column;
+		font-size: 1.5rem;
+	}
+	h1 {
+		margin-bottom: 20px;
+	}
+
+	label {
+		text-align: left;
+	}
+
+	input {
+		line-height: 3rem;
+		font-size: 2rem;
+		margin-bottom: 20px;
+		width: 100%;
+		padding: 0;
+	}
+
+	.join-button {
+		text-align: center;
+		font-size: 2rem;
+		margin-top: 20px;
+		display: block;
+		padding: 1rem 0;
+		width: 100%;
+		background-color: #eeeeee;
+		color: #333;
+		text-transform: uppercase;
+	}
+</style>
